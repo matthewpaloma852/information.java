a88a3e473a44947550a5af88ea60624f69005bce
matthewpaloma852 <92876304+matthewpaloma852@users.noreply.github.com>
2022-03-24 19:14:34 +0800

    This file for output information in java

---
+++ Information.java
@@ -0,0 +1,18 @@
+class information{
+    
+	public static void main(String args[]){
+     
+	 System.out.print("First Name: ");
+    System.out.println("Matthew");
+	System.out.print("Middle Name: ");
+    System.out.println("Rondina");
+    System.out.print("Last Name: ");
+    System.out.println("Paloma");
+    System.out.print("Adress: ");
+    System.out.println("Humay-humay Gun-ob Lapu-lapu City");
+    System.out.print("Course and Year: ");
+    System.out.println("BSIS 1-2B");
+    System.out.print("Subject: ");
+    System.out.println("Programming 2");
+	}
+}
